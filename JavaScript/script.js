d6363a8f776fafef2bbba06b045da2c649642f5f
subrata-467 <subratabera467@gmail.com>
2024-08-01 23:30:43 +0530

    change p

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,15 +2,14 @@ let weight = document.querySelector("#txtweight");
 let height = document.querySelector("#txtheight");
 let btn = document.querySelector("button");
 let main = document.querySelector(".main");
+let p = document.querySelector("p");
 
 function CalculateBMI(){
     let height_f = parseFloat(height.value);
     let height_m = height_f * 0.3048;
     let weight_int = parseFloat(weight.value);
     let bmi = weight_int / (height_m * height_m);
-    let para = document.createElement("p");
-    para.innerText = `Your BMI is ${bmi.toFixed(2)}`;
-    main.appendChild(para);
+    p.innerText = `Your BMI is ${bmi.toFixed(2)}`;
 }
 
 btn.addEventListener("click", () => {
